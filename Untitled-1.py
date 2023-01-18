7f9befe32698a3661a218a524e0621cce0433c32
Avaladez1 <antoniovaladez03@gmail.com>
2023-01-17 18:05:09 -0800

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,8 @@
 class Student:
-    def __init__(self, slides_seen, driving_speed):
+    def __init__(self, slides_seen, driving_speed, start_time):
         self.slides_seen = slides_seen
         self.driving_speed = driving_speed
+        self.start_time = start_time
         self.next = None
+
+
