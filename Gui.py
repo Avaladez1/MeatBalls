870cded6bff4cb239deccb2e4f0241ab0abbb791
Avaladez1 <antoniovaladez03@gmail.com>
2023-02-09 17:23:07 -0800

    broken

--- Gui.py
+++ Gui.py
@@ -21,11 +21,25 @@ background_label.pack(fill="both", expand="yes")
 text1 = tk.Entry(background_label, fg='black', width=8)
 text1.place(x=40,y=0)
 
+
+def set_students():
+    Sim.number_of_students=text1.get()
+
+
+
+
+
 button1 = tk.Button(background_label, text="Number_Students", fg='black', height=2, width=10)
 button1.place(x=20,y=50)
 
 text2 = tk.Entry(background_label, fg='black', width=8)
 text2.place(x=295,y=0)
+
+def slide_amount_change():
+    Sim.number_of_students=text2.get()
+
+
+
 
 button2 = tk.Button(background_label, text="Slide_Amount", fg='black', height=2, width=10)
 button2.place(x=280,y=50)
@@ -33,17 +47,25 @@ button2.place(x=280,y=50)
 text3 = tk.Entry(background_label, fg='black', width=8)
 text3.place(x=560, y=0)
 
+def Slide_length_change():
+    Sim.slide_length
+
 button3 = tk.Button(background_label, text="Slide_Length", fg='black', height=2, width=10)
 button3.place(x=540,y=50)
 
 text4 = tk.Entry(background_label, fg='black', width=8)
 text4.place(x=820,y=0)
 
+def set_drive_time():
+    Sim.student.driving_time
+
 button4 = tk.Button(background_label, text="Drive_Time", fg='black', height=2, width=10)
 button4.place(x=803,y=50)
 
 button5 = tk.Button(background_label, text="Submit", fg='black', height=2, width=15)
 button5.place(x=390,y=140)
+
+def Submit():
 
 
 root.mainloop()
